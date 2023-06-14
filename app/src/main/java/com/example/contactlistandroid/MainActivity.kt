fa5704e9efb69d00c068ebec20febdb3ba314c85
martinprodeveloper <martinprodeveloper@gmail.com>
2023-06-14 15:33:29 -0500

    Volley

--- app/src/main/java/com/example/contactlistandroid/MainActivity.kt
+++ app/src/main/java/com/example/contactlistandroid/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.contactlistandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerViewContact: RecyclerView
+    lateinit var dataModelArrayList: ArrayList<Contact>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +24,37 @@ class MainActivity : AppCompatActivity() {
             Contact("Lucas", "333222111")
         )
 
+        dataModelArrayList = ArrayList()
+
         recyclerViewContact = findViewById(R.id.rv_contact)
-        recyclerViewContact.layoutManager = LinearLayoutManager(this)
-        recyclerViewContact.adapter = ContactAdapter(contacts)
+        // recyclerViewContact.layoutManager = LinearLayoutManager(this)
+        // recyclerViewContact.adapter = ContactAdapter(contacts)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://jsonplaceholder.typicode.com/users"
+
+        val jsonArrayRequest = JsonArrayRequest(url,
+            Response.Listener { response ->
+                Log.i("Volley", "response: $response")
+                for (i in 0 until response.length()) {
+                    val item = response.getJSONObject(i)
+                    Log.i("Volley", "item: $item")
+                    dataModelArrayList.add(
+                        Contact(
+                            item.get("name").toString(),
+                            item.get("phone").toString()
+                        )
+                    )
+                }
+                recyclerViewContact.layoutManager = LinearLayoutManager(this)
+                recyclerViewContact.adapter = ContactAdapter(dataModelArrayList)
+            },
+            Response.ErrorListener { error ->
+                error.printStackTrace()
+            }
+        )
+
+        queue.add(jsonArrayRequest)
 
     }
 }
